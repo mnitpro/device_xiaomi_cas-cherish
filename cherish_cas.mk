3537b5664180b6202b48021a89b50e8383ec5d0c
Mahmoud Nabeel <mnitpro@gmail.com>
2022-09-11 19:38:57 +0300

    cas: CherishOS bringups

--- cherish_cas.mk
+++ cherish_cas.mk
@@ -10,15 +10,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from the device configuration.
 $(call inherit-product, device/xiaomi/cas/device.mk)
+# Inherit some common sweet stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
-# Inherit from the cherishUI configuration.
-$(call inherit-product, vendor/cherish/config/mobile.mk)
+# GApps
+$(call inherit-product-if-exists, vendor/gapps/core/config.mk)
 
 PRODUCT_NAME := cherish_cas
 PRODUCT_DEVICE := cas
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2007J1SC
 PRODUCT_MANUFACTURER := Xiaomi
+BOARD_VENDOR := Xiaomi
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# FOD animations
+EXTRA_UDFPS_ANIMATIONS := true
+CHERISH_BUILD_TYPE := UNOFFICIAL
 
 WITH_GMS_CORE := true
 
